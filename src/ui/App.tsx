fc8e6c9af7bf85854593e6f2f73740d505439092
FrancoGargaglioni <FrancoGargaglioni@gmail.com>
2025-02-11 15:56:45 -0300

    Added a few fs functions to test, created develop branch

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <>
       <div>
-        1
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
